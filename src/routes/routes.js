d40ccd6baa4ed82d46638662fc4a15aeb48939d4
Emmanuel Menyaga <sparklinsparky@gmail.com>
2020-07-15 21:32:01 +0100

    fix routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,10 +5,16 @@ const Validation = require("../utils/Validator");
 const Controller = require("../controllers/Controller");
 
 router.post(
-  "/v1/manipulate",
-  Validation.routeValidation(),
-  Controller.manipulateImage()
+  "/v1/resize",
+  Validation.resizingValidation(),
+  Controller.resizeImage()
 );
+router.post(
+  "/v1/crop",
+  Validation.croppingValidation(),
+  Controller.cropImage()
+);
+router.post("/v1/rotate", Validation.rotationValidation(), Controller.rotate());
 
 router.use("/v1/static", express.static(path.join(__dirname, "../images")));
 
